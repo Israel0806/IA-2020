0c0fd7f9937a6327b91e18af17c09fda0684de58
Neo <31198370+GuidoTapia@users.noreply.github.com>
2020-06-06 13:35:20 -0500

    Add files via upload

---
+++ 4. Laboratorio tema de redes neuronales/creadorPatrones/main.cpp
@@ -0,0 +1,147 @@
+#ifdef __APPLE__
+# include <OpenGL/gl.h>
+# include <OpenGL/glu.h>
+# include <GLUT/glut.h>
+#else
+# include <GL/gl.h>
+# include <GL/glu.h>
+# include <GL/glut.h>
+#endif
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+char matriz[20][20]={};
+int filas=10;
+int columnas=5;
+
+void reshape_cb (int w, int h) {
+	if (w==0||h==0) return;
+	glViewport(0,0,w,h);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	gluOrtho2D(0,w,0,h);
+	glMatrixMode (GL_MODELVIEW);
+	glLoadIdentity ();
+}
+
+void display_cb() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	for (int i=0;i<filas;++i){
+		for (int j=0;j<columnas;++j){
+			if(matriz[filas-i-1][j]=='0'){
+				glColor3f(1,1,1); glLineWidth(3);
+			}else{
+				glColor3f(0,0,0); glLineWidth(3);
+			}
+			glBegin(GL_POLYGON);
+			
+				glVertex2d(70*j,70*i);
+				glVertex2d(70*(j+1),70*i);
+				glVertex2d(70*(j+1),70*(i+1));
+				glVertex2d(70*j,70*(i+1));
+			
+			glEnd();
+		}
+	}
+	
+	glutSwapBuffers();
+}
+
+
+GLvoid window_key (unsigned char key, int x, int y) {
+	ofstream myfile;
+	bool flag=0;
+	string archivo;
+	switch ( key ) {
+	case 27:
+		exit (0);
+		break;
+	case '1':
+		archivo="1.txt";
+		flag=1;
+		break;
+	case '2':
+		archivo="2.txt";
+		flag=1;
+		break;
+	case '3':
+		archivo="3.txt";
+		flag=1;
+		break;
+	case '4':
+		archivo="4.txt";
+		flag=1;
+		break;
+	case '5':
+		archivo="5.txt";
+		flag=1;
+		break;
+	case '6':
+		archivo="6.txt";
+		flag=1;
+		break;
+	case '7':
+		archivo="7.txt";
+		flag=1;
+		break;
+	case '8':
+		archivo="8.txt";
+		flag=1;
+		break;
+	case '9':
+		archivo="9.txt";
+		flag=1;
+		break;
+	case '0':
+		archivo="0.txt";
+		flag=1;
+		break;
+	}
+	if(flag){
+		ofstream myfile(archivo,  ios::out | ios::app);
+		myfile << filas<<' '<<columnas<<endl;
+		for(int i=0;i<filas;++i)
+			myfile <<matriz[i]<<endl;
+		myfile.close();
+	}
+	glutPostRedisplay();
+}
+
+
+GLvoid OnMouseClick (int button, int state, int x, int y) {
+	if ( (button == GLUT_LEFT_BUTTON or button == GLUT_RIGHT_BUTTON) && state == GLUT_DOWN ) {		
+		x/=70;
+		y/=70;
+		cout<<y<<"  "<<x<<endl;
+		matriz[y][x]=(matriz[y][x]=='0')?'1':'0';
+	}
+	glutPostRedisplay ();
+}
+
+void initialize() {
+	glutInitDisplayMode (GLUT_RGBA|GLUT_DOUBLE);
+	glutInitWindowSize (columnas*70,filas*70);
+	glutInitWindowPosition (100,100);
+	glutCreateWindow ("Ventana OpenGL");
+	glutDisplayFunc (display_cb);
+	glutReshapeFunc (reshape_cb);
+	glutKeyboardFunc(&window_key);
+	glutMouseFunc (&OnMouseClick);
+	glClearColor(1.f,1.f,1.f,1.f);
+}
+
+int main (int argc, char **argv) {
+	
+	cout<<"Ingresa el numero de filas: ";
+	cin>>filas;
+	cout<<"Ingresa el numero de columnas: ";
+	cin>>columnas;
+	for (int i=0;i<filas;++i)
+		for (int j=0;j<columnas;++j)
+			matriz[i][j]='0';
+	glutInit (&argc, argv);
+	initialize();
+	glutMainLoop();
+	return 0;
+}
---
+++ 4. Laboratorio tema de redes neuronales/perceptron/main.cpp
@@ -0,0 +1,215 @@
+#ifdef __APPLE__
+# include <OpenGL/gl.h>
+# include <OpenGL/glu.h>
+# include <GLUT/glut.h>
+#else
+# include <GL/gl.h>
+# include <GL/glu.h>
+# include <GL/glut.h>
+#endif
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <bits/stdc++.h> 
+#include <math.h>
+#include <string>
+
+using namespace std;
+bool matriz[20][20]={};
+int filas;
+int columnas;
+
+struct perceptron{
+	int validos[10];
+	vector<double> pesos[10];
+	double bias;
+	void extraerDatos(int i, vector<vector<bool> > &matricesEntrenamiento, vector<int> &correspondiente){
+		validos[i]=0;
+		ifstream ifs;
+		ifs.open ( to_string(i)+".txt", std::ifstream::in);
+		string aux;
+		ifs>>aux;
+		while (ifs.good()) {
+			int n= stoi(aux);
+			ifs>>aux;
+			int m= stoi(aux);
+			if(n==filas && m==columnas){
+				validos[i]++;
+				matricesEntrenamiento.push_back(vector<bool>(filas*columnas,0));
+				correspondiente.push_back(i);
+				for(int j=0;j<n;++j){
+					ifs>>aux;
+					cout << aux<<endl;
+					for(int k=0;k<m;++k){
+						matricesEntrenamiento.back()[j*m+k]=(aux[k]=='1');
+					}
+				}
+			}else{
+				for(int j=0;j<n;++j){
+					ifs>>aux;
+				}
+			}
+			ifs>>aux;
+		}
+		ifs.close();
+	}
+	void entrenar(double tasaAprendizaje){
+		bias=0.0;
+		for(int i=0;i<10;++i){
+			pesos[i].resize(filas*columnas,0.0);
+		}
+		
+		vector<vector<bool> > matricesEntrenamiento;
+		vector<int> correspondiente;
+		for(int i=0;i<10;++i){
+			extraerDatos(i,matricesEntrenamiento,correspondiente);
+		}
+		for(int i=0;i<10;++i){
+			//cout<<endl<<i<<endl;
+			if(validos[i]>0){
+				bool algunoNoCoincide=true;
+				//cout<<endl<<validos[i]<<endl;
+				while(algunoNoCoincide){
+					//cout<<endl<<1<<endl;
+					//cout<<endl<<'a'<<endl;
+					algunoNoCoincide=false;
+					for(auto it=0;it<matricesEntrenamiento.size();it++){
+						//cout<<endl<<'a'<<endl;
+						//cout<<endl<<matricesEntrenamiento[it].size()<<endl;
+						//cout<<endl<<2<<endl;
+						double resultado=bias;
+						//cout<<endl<<pesos[i].size()<<endl;
+						for(int j=0;j<matricesEntrenamiento[it].size();++j){
+							resultado+=matricesEntrenamiento[it][j]*pesos[i][j];
+							
+						}
+						//cout<<endl<<'b'<<endl;
+						bool y=(resultado>0);
+						if(y!=(i==correspondiente[it]) ){
+							algunoNoCoincide=true;
+							//reajuste
+							int diferencia=1;
+							if(y)
+								diferencia=-1;
+							
+							bias+=diferencia;
+							for(int j=0;j<matricesEntrenamiento[it].size();++j){
+								pesos[i][j]+=diferencia*matricesEntrenamiento[it][j];
+							}
+						}
+					}	
+				}
+				cout<<i<<" Entrenado";
+			}
+		}
+	}
+	vector<int> reconocer(){
+		vector<int> posibles;
+		for(int k=0;k<10;++k){
+			if(validos[k]>0){
+				double resultado=bias;
+				for(int i=0;i<filas;++i){
+					for(int j=0;j<columnas;++j){
+						resultado+=pesos[k][i*columnas+j]*matriz[i][j];
+					}
+				}
+				if(resultado>0)
+					posibles.push_back(k);
+			}
+		}
+		return posibles;
+	}
+};
+
+perceptron percep;
+
+
+void reshape_cb (int w, int h) {
+	if (w==0||h==0) return;
+	glViewport(0,0,w,h);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity ();
+	gluOrtho2D(0,w,0,h);
+	glMatrixMode (GL_MODELVIEW);
+	glLoadIdentity ();
+}
+
+
+
+
+
+void display_cb() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	for (int i=0;i<filas;++i){
+		for (int j=0;j<columnas;++j){
+			glColor3f(0,0,0); glLineWidth(3);
+			glBegin(GL_POLYGON);
+			glVertex2d(70*j,70*i);
+			glVertex2d(70*(j+1),70*i);
+			glVertex2d(70*(j+1),70*(i+1));
+			glVertex2d(70*j,70*(i+1));
+			glEnd();
+			if(!matriz[filas-i-1][j]){
+				glBegin(GL_POLYGON);
+				glColor3f(1,1,1); glLineWidth(3);
+				glVertex2d(70*j+2,70*i+2);
+				glVertex2d(70*(j+1)-2,70*i+2);
+				glVertex2d(70*(j+1)-2,70*(i+1)-2);
+				glVertex2d(70*j+2,70*(i+1)-2);
+				glEnd();
+			}
+		}
+	}
+	vector<int> posibles=percep.reconocer();
+	glColor3f(1,0,0); glLineWidth(3);
+	for(int i=0;i<posibles.size(); ++i){
+		cout<<posibles[i]<<endl;
+		glRasterPos3f(70*columnas+10, (filas*35)-i*50, 0);
+		string aux="Se Reconoce: " +to_string(posibles[i]);
+		for (int j = 0; j < aux.length(); j++) {
+			glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, aux[j]);  // Updates the position
+		}
+	}
+	glutSwapBuffers();
+}
+
+
+
+
+
+GLvoid OnMouseClick (int button, int state, int x, int y) {
+	if ( (button == GLUT_LEFT_BUTTON or button == GLUT_RIGHT_BUTTON) && state == GLUT_DOWN ) {		
+		x/=70;
+		y/=70;
+		//cout<<y<<"  "<<x<<endl;
+		matriz[y][x]= !matriz[y][x];
+	}
+	glutPostRedisplay ();
+}
+
+void initialize() {
+	glutInitDisplayMode (GLUT_RGBA|GLUT_DOUBLE);
+	glutInitWindowSize (columnas*70+200,filas*70);
+	glutInitWindowPosition (100,100);
+	glutCreateWindow ("Ventana OpenGL");
+	glutDisplayFunc (display_cb);
+	glutReshapeFunc (reshape_cb);
+	//glutKeyboardFunc(&window_key);
+	glutMouseFunc (&OnMouseClick);
+	glClearColor(1.f,1.f,1.f,1.f);
+}
+
+int main (int argc, char **argv) {
+	
+	cout<<"Ingresa el numero de filas: ";
+	cin>>filas;
+	cout<<"Ingresa el numero de columnas: ";
+	cin>>columnas;
+	percep.entrenar(0.5);
+	glutInit (&argc, argv);
+	initialize();
+	glutMainLoop();
+	return 0;
+}
+
